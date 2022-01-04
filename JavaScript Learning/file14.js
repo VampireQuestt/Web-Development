237b875f9f8ff6f778d7341e9beba20d7b97d181
Deepak Kumar <75717057+VampireQuestt@users.noreply.github.com>
2022-01-04 23:29:04 +0530

    Arrays using const keyword

---
+++ JavaScript Learning/file14.js
@@ -0,0 +1,12 @@
+console.log("__________ const keyword for creating Arrays ___________\n\n");
+
+// using const for creating Array
+
+// const pi = 3.14;
+// pi = 10;
+// console.log(pi);
+
+const arr = ["apple" , "grapes"];   // actual array is stored somewhere in heap
+arr.push("banana");
+// arr = ["vampire_questt" , "Invincible327"];    // you can't redefine const Array
+console.log(arr);
