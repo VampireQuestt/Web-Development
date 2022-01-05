39d6498ee05cbb72a6db99c1ad3de2d56892e8df
Deepak Kumar <75717057+VampireQuestt@users.noreply.github.com>
2022-01-05 23:54:18 +0530

    Computed Property in Objects

---
+++ JavaScript Learning/file18.js
@@ -0,0 +1,25 @@
+// computed properties
+
+const key1 = "name";
+const key2 = "age";
+
+const value1 = "Deepak";
+const value2 = 20;
+
+// const obj = {
+//     key1 : value1,
+//     key2 : value2
+// };
+
+// const obj = {
+// };
+
+// obj[key1] = value1;
+// obj[key2] = value2;
+
+const obj = {
+    [key1] : value1,
+    [key2] : value2
+};
+
+console.log(obj);
